7d75fbe81acf92d499cf6ede9d32fa828b9e1eb6
lspaudel <lspaudel@gitlab.com>
2026-02-17 00:52:45 +0545

    feat: enhance admin interfaces for Blog and CustomUser
    Blog admin:
    - Add custom list display with all key fields
    - Add filters for category, date, and deletion status
    - Add search by title, content, and username
    - Add auto-populate slug from title
    - Organize fields with fieldsets
    - Add custom actions for soft delete and restore
    
    CustomUser admin:
    - Extend UserAdmin with custom fields
    - Display additional fields in list view
    - Add custom fieldsets for bio, quote, and profile_pic

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'created_at')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'created_at')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('created_at', 'updated_at', 'last_login', 'date_joined')
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information', {
+            'fields': ('bio', 'quote', 'profile_pic', 'created_at', 'updated_at')
+        }),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Information', {
+            'fields': ('email', 'bio', 'quote', 'profile_pic')
+        }),
+    )
+
--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'category', 'created_at', 'updated_at', 'is_deleted')
+    list_filter = ('category', 'created_at', 'is_deleted')
+    search_fields = ('title', 'content', 'user__username')
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('user', 'title', 'slug', 'category')
+        }),
+        ('Content', {
+            'fields': ('content', 'image')
+        }),
+        ('Metadata', {
+            'fields': ('created_at', 'updated_at', 'is_deleted'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    actions = ['soft_delete_selected', 'restore_selected']
+    
+    def soft_delete_selected(self, request, queryset):
+        """Soft delete selected blogs"""
+        for blog in queryset:
+            blog.soft_delete()
+        self.message_user(request, f'{queryset.count()} blog(s) soft deleted.')
+    soft_delete_selected.short_description = 'Soft delete selected blogs'
+    
+    def restore_selected(self, request, queryset):
+        """Restore selected blogs"""
+        for blog in queryset:
+            blog.restore()
+        self.message_user(request, f'{queryset.count()} blog(s) restored.')
+    restore_selected.short_description = 'Restore selected blogs'
+
